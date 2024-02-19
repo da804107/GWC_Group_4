fdde7425f47a88a4331e25d07f5b3662c997a4a5
da804107 <114943299+da804107@users.noreply.github.com>
2024-02-19 22:42:10 +0000

    This is the first commit of our project. I was just testing out the online version of VSCode and how it works for nltk package as it doesn't work on my computer directly.

---
+++ nlpProj.py
@@ -0,0 +1,32 @@
+# Import necessary modules from NLTK
+import nltk
+from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+
+# Download the necessary NLTK data (only needed once)
+nltk.download('punkt')
+nltk.download('stopwords')
+
+# Sample text to be tokenized
+text = "Hello there! How are you today? This is an example of sentence tokenization."
+
+# Sentence Tokenization
+sentences = sent_tokenize(text)
+print("Sentence Tokenization:")
+print(sentences)
+
+# Word Tokenization
+words = word_tokenize(text)
+print("\nWord Tokenization:")
+print(words)
+
+stopWords = set(stopwords.words('english'))
+
+filteredList = []
+
+for word in words:
+    if word.casefold() not in stopWords:
+        filteredList.append(word)
+
+print("\nFiltered Tokenization:")
+print(filteredList)
